0c244f1c96aa6617fdbbd4f5e5e8ba5e3f28af40
Dave Doolin <david.doolin@gmail.com>
2014-12-09 06:04:28 -0800

    more filter code, p. 42

--- filters.js
+++ filters.js
@@ -1,4 +1,6 @@
 app = angular.module('myApp', []);
 
 app.controller('FilterDemoController', function($scope) {
+  $scope.isCapitalized =
+    function(str) { return str[0] == str[0].toUpperCase(); }
   });
