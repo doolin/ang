6054625950fde1357a15e852cc7dcce28fd47d9f
Dave Doolin <david.doolin@gmail.com>
2014-12-12 06:41:21 -0800

    non-working custom filter example p. 45

--- filters.js
+++ filters.js
@@ -4,3 +4,15 @@ app.controller('FilterDemoController', function($scope) {
   $scope.isCapitalized =
     function(str) { return str[0] == str[0].toUpperCase(); }
   });
+
+  // example on page 45 doesn't work with what I have so far
+angular.module('myApp.filters', [])
+  .filter('capitalize', function() {
+    return function(input) {
+      if (input)
+        return input[0].toUpperCase() +
+          input.slice(1);
+    }
+  });
+
+
