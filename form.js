59fc0af08e8e023c3c013bb9d6d2d81be9c80fe6
Dave Doolin <david.doolin@gmail.com>
2014-12-20 06:32:39 -0800

    add js to form validation, p. 54

--- form.js
+++ form.js
@@ -3,3 +3,26 @@ app = angular.module('myApp', []);
 app.controller('FormDemoController',
   function($scope) {
   });
+
+app.directive('ensureUnique', function($http) {
+  return {
+    require: 'ngModel',
+    link: function(scope, ele, attrs, c) {
+      scope.$watch(attrs.ngModel, function(n) {
+        if (!n) return;
+        $http({
+          method: 'POST',
+          url: '/api/check/'+attrs.ensureUnique,
+          data: {
+            field: attrs.ensureUnique,
+            value: scope.ngModel
+          }
+        }).success(function(data) {
+          c.$setValidity('unique', data.isUnique);
+        }).error(function(data) {
+          c.$setValidity('unique', false);
+        });
+      });
+    }
+  }
+});
