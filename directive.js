d6a6eb03c38b18f0679a5102ecd00d94bdfc68e5
Dave Doolin <david.doolin@gmail.com>
2015-01-04 06:07:31 -0800

    directives, p. 70

--- directive.js
+++ directive.js
@@ -4,4 +4,11 @@ angular.module('myApp', [])
     restrict: 'E',
     template: '<a href="http://google.com">Click me to go to Google</a>'
   }
+})
+.directive('myDirective2', function() {
+  return {
+    restrict: 'E',
+    replace: true,
+    template: '<a href="http://google.com">Click me to go to Google</a>'
+  }
 });
